11452a5c455cfdadb016abff817d2be762281083
Sho Haneishi <haneishi0715@gmail.com>
2023-12-09 19:42:34 +0900

    change of output time

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -9,7 +9,7 @@ class Talker():
     def __init__(self, node):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
-        node.create_timer(0.5, self.cb)
+        node.create_timer(1.0, self.cb)
 
     def cb(self):
         msg = Int16()
